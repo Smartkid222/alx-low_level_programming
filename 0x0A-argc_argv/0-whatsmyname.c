4f768fd97fd83553f4cb91ca7bca02713f69eff4
Smartkid222 <emekam848@gmail.com>
2023-08-04 03:36:10 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - program that prints its name
+ * @argc: program 1
+ * @argv: program 2
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}

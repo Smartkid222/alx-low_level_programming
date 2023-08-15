519f72dfdc28257b6be0da0bb27d555809f43f16
Smartkid222 <emekam848@gmail.com>
2023-08-15 04:19:38 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - function that define a new type
+ * @name: element 1
+ * @age: element 2
+ * @owner: element 3
+ * Return: 0
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
